94248e41bba3af324d5d84d44f4b7e05d4d1d2e2
Million Alemayehu <78154106+Million-Alemayehu@users.noreply.github.com>
2026-02-01 16:38:47 +0300

    react hook and conditional reandering

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import './index.css';
+
 
 const Card =({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title}" has been ${hasLiked ? "liked" : "unliked"}.`);
+  }, [hasLiked, title]);
+
+
   return (
-    <div className="card" >
-      <h2>{title}</h2>
+    <div className="card" onClick={()=>setCount(count+ 1)}>
+      <h2>{title}<br/>{count || null}</h2>
+
+      <button onClick = {() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "🧡" : "🤍"}
+      </button>
     </div>  
   )
 }
 
 
 const App = () => {
+  
+
   return (
     <div classname= "card-container">
       <Card title="Star Wars" rating= {5} isCool={true} />
